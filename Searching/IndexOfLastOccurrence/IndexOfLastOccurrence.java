6dfd694ee8781a19ae4b9916cfa25b9644069882
Vijayalakshmi R <109212696+VijayalakshmiRaju@users.noreply.github.com>
2025-08-17 20:26:15 +0530

    covered edge cases in finding last occurrence

--- Searching/IndexOfLastOccurrence/IndexOfLastOccurrence.java
+++ Searching/IndexOfLastOccurrence/IndexOfLastOccurrence.java
@@ -17,6 +17,7 @@ public class IndexOfLastOccurrence {
     {
         int low = 0;
         int high = arr.length - 1;
+        int result = -1;
 
         while ( low <= high )
         {
@@ -26,19 +27,17 @@ public class IndexOfLastOccurrence {
             else if ( arr[mid] > x )
                 high = mid - 1; // move left
             else {
-                if (arr[mid] == x && (arr[mid + 1] != x))
-                    return mid;
-                else
-                    low = mid + 1;
+                result =  mid;
+                low = mid + 1;
             }
         }
 
-        return -1;
+        return result;
     }
 
     // Main method for testing
     public static void main(String[] args) {
-        int[] arr = {1, 2, 2, 2, 3, 4, 5, 5, 5, 6};
+        int[] arr = {1, 2, 2, 2, 3, 4, 5, 5, 5, 5};
 
         int x1 = 2;
         int x2 = 5;
