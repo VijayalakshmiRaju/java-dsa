43582aa8fda1eb0b47dc8da3cc93c5313279c01a
Vijayalakshmi R <109212696+VijayalakshmiRaju@users.noreply.github.com>
2025-10-06 23:02:03 +0530

    Create CountSetBits.java

---
+++ BitMagic/CountSetBits/CountSetBits.java
@@ -0,0 +1,12 @@
+public class CountSetBits {
+    public static int countSetBits ( int n )
+    {
+        int count = 0;
+        while ( n != 0 ) {
+            count += n & 1;
+            n >>= 1;
+        }
+
+        return count;
+    }
+}
