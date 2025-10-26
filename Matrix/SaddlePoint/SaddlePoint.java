ddfd772dc5149e3ecb534e46446ab2a2cb124819
Vijayalakshmi R <109212696+VijayalakshmiRaju@users.noreply.github.com>
2025-10-26 21:10:14 +0530

    Update SaddlePoint.java

--- Matrix/SaddlePoint/SaddlePoint.java
+++ Matrix/SaddlePoint/SaddlePoint.java
@@ -4,27 +4,39 @@ public class SaddlePoint {
         int rows = matrix.length;
         int columns = matrix[0].length;
 
-        int[] rowMinimum = new int[rows];
         for(int i = 0; i < rows; i++ ) {
-            int ele = matrix[i][0];
+            int minValue = matrix[i][0];
+            int colIndex = 0;
+
             for ( int j = 1; j < columns; j++ ) {
-                ele = Math.min(ele, matrix[i][j]);
-            }
-
-            rowMinimum[i] = ele;
-        }
-
-        for (int col = 0; col < columns; col++ ) {
-            int ele = matrix[0][col];
-            for ( int row = 1; row < rows; row++ ) {
-                ele = Math.max(ele, matrix[row][col]);
-
-                if (row == rows - 1 && rowMinimum[row] == ele) {
-                    return rowMinimum[row];
+                if (matrix[i][j] < minValue) {
+                    minValue = matrix[i][j];
+                    colIndex = j;
                 }
             }
+
+            boolean isSaddle = true;
+            for ( int k = 0; k < rows; k++ ) {
+                if ( matrix[k][colIndex] > minValue ) {
+                    isSaddle = false;
+                    break;
+                }
+            }
+
+            if (isSaddle)
+                return minValue;
         }
 
         return -1;
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = {
+                {4, 5, 6},
+                {7, 8, 9},
+                {1, 2, 3}
+        };
+
+        System.out.println(findSaddlePoint(matrix)); // Output: 7
     }
 }
